d5fc2536ae72cfba03e35cd71482a3e9a59af8f8
PrashantGuptacodecraft <adityagupta983869@gmail.com>
2026-04-09 00:51:32 +0530

    updated

---
+++ grocify/src/Components/Products/Products.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Heading from '../Heading/Heading'
+
+const Products = () => {
+  return (
+    <section className='max-w-[1400px] mx-auto px-10 py-20'>
+<Heading highlight="Our" heading="Products"></Heading>
+    </section>
+  )
+}
+
+export default Products
